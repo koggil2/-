e2d8bd6da0398d9c447b586442fa1f1adcae1ca3
shaqed89 <53854917+shaqed89@users.noreply.github.com>
2019-11-15 09:50:35 +0900

    Revert "커밋"
    This reverts commit 79c4d8ad59b57eb1fb49097240cf758cf132aa8e.

--- src/main/java/kr/goott/tour/register/RegisterController.java
+++ src/main/java/kr/goott/tour/register/RegisterController.java
@@ -1,5 +1,7 @@
 package kr.goott.tour.register;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
